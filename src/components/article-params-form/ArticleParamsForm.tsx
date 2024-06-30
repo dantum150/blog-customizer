67a1b5283164ecb85936cc5c440297ac543bef83
dantum <dantum150@yandex.ru>
2024-06-30 08:51:50 +0300

    30/06/2024

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,32 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
-
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
+import { ReactNode } from 'react';
 
-export const ArticleParamsForm = () => {
+// props: {changeAside, isVisible}
+export const ArticleParamsForm = (props: {
+	changeAside: () => void;
+	changePageVisual: (event: React.FormEvent<HTMLFormElement>) => void;
+	reset: () => void;
+	isVisible: boolean;
+	children: ReactNode;
+}) => {
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton
+				changeAside={props.changeAside}
+				isVisible={props.isVisible}
+			/>
+			<aside
+				className={clsx({
+					[styles.container]: true,
+					[styles.container_open]: props.isVisible,
+				})}>
+				<form onSubmit={props.changePageVisual} className={styles.form}>
+					{props.children}
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button onClick={props.reset} title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,113 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	OptionType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
-
+import { Select } from './components/select';
+import { RadioGroup } from './components/radio-group';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const defaultStyleOptions = {
+		fontFamily: fontFamilyOptions[0],
+		fontSize: fontSizeOptions[0],
+		fontColor: fontColors[0],
+		backgroundColor: backgroundColors[0],
+		width: contentWidthArr[0],
+	};
+
+	const [options, setOptions] = useState(defaultStyleOptions);
+
+	const [isVisible, setIsVisible] = useState(false);
+
+	const [style, setStyle] = useState(defaultStyleOptions);
+
+	function onClick() {
+		setIsVisible(!isVisible);
+	}
+
+	function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		setStyle(options);
+	}
+
+	function onChange(key: string, option: OptionType) {
+		setOptions({ ...options, [key]: option });
+	}
+
+	function reset() {
+		setOptions(defaultStyleOptions);
+		setStyle(defaultStyleOptions);
+	}
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': style.fontFamily.value,
+					'--font-size': style.fontSize.value,
+					'--font-color': style.fontColor.value,
+					'--container-width': style.width.value,
+					'--bg-color': style.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				changeAside={onClick}
+				isVisible={isVisible}
+				changePageVisual={onSubmit}
+				reset={reset}>
+				<div className={styles.selectGroup}>
+					<h2 className={styles.title}>Задайте параметры</h2>
+					<Select
+						selected={options.fontFamily}
+						options={fontFamilyOptions}
+						title='шрифт'
+						onChange={(option) => onChange('fontFamily', option)}
+					/>
+					<RadioGroup
+						title='размер шрифта'
+						selected={options.fontSize}
+						options={fontSizeOptions}
+						name='размер шрифта'
+						onChange={(option) => onChange('fontSize', option)}
+					/>
+
+					<Select
+						selected={options.fontColor}
+						options={fontColors}
+						title='цвет шрифта'
+						onChange={(option) => onChange('fontColor', option)}
+					/>
+				</div>
+
+				<div className={styles.selectGroup}>
+					<Select
+						selected={options.backgroundColor}
+						options={backgroundColors}
+						title='цвет фона'
+						onChange={(option) => onChange('backgroundColor', option)}
+					/>
+					<Select
+						selected={options.width}
+						options={contentWidthArr}
+						title='ширина контента'
+						onChange={(option) => onChange('width', option)}
+					/>
+				</div>
+			</ArticleParamsForm>
 			<Article />
 		</div>
 	);
